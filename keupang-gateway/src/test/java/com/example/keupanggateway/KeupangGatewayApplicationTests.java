2eddb72bf29f8824f7a80d7decec61b9e05b60fd
JongHak19 <cj855695@gmail.com>
2024-11-26 00:30:13 +0900

    api-gateway-create

---
+++ keupang-gateway/src/test/java/com/example/keupanggateway/KeupangGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.keupanggateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KeupangGatewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
