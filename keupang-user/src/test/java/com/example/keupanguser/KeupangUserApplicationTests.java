9273ab05c7089636d09df21cd576381077898d7b
JongHak19 <cj855695@gmail.com>
2024-11-25 18:59:27 +0900

    user service create

---
+++ keupang-user/src/test/java/com/example/keupanguser/KeupangUserApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.keupanguser;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KeupangUserApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
