75679ac8463bbcefc81ce3db20dedbb008b6589b
JongHak19 <cj855695@gmail.com>
2024-11-27 21:22:09 +0900

    dockerfile, docker-compose.yml 생성

--- keupang-config-server/src/test/java/com/example/keupangconfigserver/KeupangConfigServerApplicationTests.java
+++ keupang-config-server/src/test/java/com/example/keupangconfigserver/KeupangConfigServerApplicationTests.java
@@ -1,13 +1,18 @@
 package com.example.keupangconfigserver;
 
+import java.util.Base64;
+import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("test")
 class KeupangConfigServerApplicationTests {
 
     @Test
     void contextLoads() {
+        // 컨텍스트 로딩 테스트
     }
 
 }
