b8deac89c112e3728f7aa7631b0957b56eb78c5a
JongHak19 <cj855695@gmail.com>
2024-12-10 02:34:13 +0900

    gateway 라우팅 설정 오류 수정

--- keupang-user/src/main/java/com/example/keupanguser/controller/AuthController.java
+++ keupang-user/src/main/java/com/example/keupanguser/controller/AuthController.java
@@ -6,6 +6,7 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.parameters.P;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -19,7 +20,7 @@ public class AuthController {
     private final JwtTokenProvider jwtTokenProvider;
 
     @PostMapping("/login")
-    public ResponseEntity<?> login(@RequestBody UserRequest userRequest){
+    public ResponseEntity<?> login(@ModelAttribute UserRequest userRequest){
         String token = jwtTokenProvider.createToken(userRequest.getUserEmail(), "USER");
         return ResponseEntity.ok(token);
     }
