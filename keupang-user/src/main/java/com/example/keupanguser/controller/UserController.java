5aef75b8895f85b06447f78d64ac4bce082506a0
JongHak19 <cj855695@gmail.com>
2025-01-07 18:47:59 +0900

    회원가입 api 수정

--- keupang-user/src/main/java/com/example/keupanguser/controller/UserController.java
+++ keupang-user/src/main/java/com/example/keupanguser/controller/UserController.java
@@ -15,7 +15,6 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -26,7 +25,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class UserController {
     private final UserService userService;
     @PostMapping("/register")
-    public ResponseEntity<?> getUsers(@RequestBody UserRequest userRequest) {
+    public ResponseEntity<?> getUsers(@ModelAttribute UserRequest userRequest) {
         log.debug(userRequest.getUserPassword());
         User user1 = userService.registerUser(userRequest);
         if(user1 == null){
