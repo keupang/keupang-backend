6b17fd48ad884cd4f37dee28908d9d6da4296697
JongHak19 <cj855695@gmail.com>
2025-01-08 16:46:39 +0900

    gateway filter 수정(threadLocal)

--- keupang-gateway/src/main/java/com/example/keupanggateway/filter/JwtAuthenticationFilter.java
+++ keupang-gateway/src/main/java/com/example/keupanggateway/filter/JwtAuthenticationFilter.java
@@ -2,6 +2,8 @@ package com.example.keupanggateway.filter;
 
 import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.Jwts;
+import java.util.HashMap;
+import java.util.Map;
 import lombok.NonNull;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpHeaders;
@@ -16,30 +18,44 @@ public class JwtAuthenticationFilter implements WebFilter {
     @Value("${jwt.secret}")
     private String jwtSecret;
 
+    //사용자 정보를 저장할 threadLocal
+    private static final ThreadLocal<Map<String, String>> userContext = new ThreadLocal<>();
+
     @Override
     @NonNull
     public Mono<Void> filter(ServerWebExchange exchange, @NonNull WebFilterChain chain) {
-        String authorizationHeader = exchange.getRequest().getHeaders().getFirst(HttpHeaders.AUTHORIZATION);
+        String authorizationHeader = exchange.getRequest().getHeaders()
+            .getFirst(HttpHeaders.AUTHORIZATION);
 
-        if(authorizationHeader == null || !authorizationHeader.startsWith("Bearer ")){
+        if (authorizationHeader == null || !authorizationHeader.startsWith("Bearer ")) {
             exchange.getResponse().setStatusCode(HttpStatus.UNAUTHORIZED);
             return exchange.getResponse().setComplete();
         }
 
         String token = authorizationHeader.substring(7); //Bearer 제거
-        try{
+        try {
             Claims claims = Jwts.parserBuilder()
-            .setSigningKey(jwtSecret.getBytes()).build()
-            .parseClaimsJws(token)
-            .getBody();
+                .setSigningKey(jwtSecret.getBytes()).build()
+                .parseClaimsJws(token)
+                .getBody();
 
-            // Role, Email 등 필요한 정보 추출
-            exchange.getAttributes().put("userEmail", claims.get("email"));
-            exchange.getAttributes().put("userRole", claims.get("role"));
-        }catch (Exception ex){
+            String email = claims.get("email", String.class);
+            String role = claims.get("role", String.class);
+
+            Map<String, String> userDetails = new HashMap<>();
+            userDetails.put("userEmail", email);
+            userDetails.put("userRole", role);
+            userContext.set(userDetails);
+        } catch (Exception ex) {
             exchange.getResponse().setStatusCode(HttpStatus.UNAUTHORIZED);
             return exchange.getResponse().setComplete();
         }
-        return chain.filter(exchange);
+        return chain.filter(exchange)
+            .doFinally(signalType -> userContext.remove());
+    }
+
+    // ThreadLocal 에서 사용자 정보 가져오기
+    public static Map<String, String> getUserContext() {
+        return userContext.get();
     }
 }
