77abeccb54f15a1713e16a5426b9fad231af09a8
JongHak19 <cj855695@gmail.com>
2025-01-11 04:49:55 +0900

    auth -> user 의존성 추가

--- keupang-auth/src/main/java/keupang/keupangauth/client/UserClient.java
+++ keupang-auth/src/main/java/keupang/keupangauth/client/UserClient.java
@@ -2,7 +2,6 @@ package keupang.keupangauth.client;
 
 import com.example.keupanguser.domain.User;
 import java.util.Optional;
-import keupang.keupangauth.response.UserDetailsResponse;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -10,4 +9,6 @@ import org.springframework.web.bind.annotation.PathVariable;
 @FeignClient(name = "user-service", url = "https://api.keupang.store")
 public interface UserClient {
 
+    @GetMapping("/api/user/{email}")
+    Optional<User> findByUserEmail(@PathVariable String email);
 }
