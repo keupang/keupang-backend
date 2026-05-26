30a8da2a9a51df58a4965829fa0f6a2c044b5906
최종학 <cj855695@gmail.com>
2026-05-26 22:02:02 +0900

    refactor: route feign clients through eureka

--- keupang-auth/src/main/java/keupang/keupangauth/client/UserClient.java
+++ keupang-auth/src/main/java/keupang/keupangauth/client/UserClient.java
@@ -6,7 +6,7 @@ import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
-@FeignClient(name = "user-service", url = "https://api.keupang.store/api/user")
+@FeignClient(name = "user", path = "/api/user")
 public interface UserClient {
 
     @GetMapping("/jwt/{email}")
